ef3db8bf7b07a20a596e2b002c19bedf58121eac
Martin Chin <martinresx@gmail.com>
2018-07-20 17:08:28 +0800

    add

--- passport.js
+++ passport.js
@@ -16,6 +16,7 @@ module.exports = (app) => {
         profileFields: ['id', 'displayName', 'name', 'gender', 'email', 'picture','profileUrl']
       },
       function(accessToken, refreshToken, profile, cb) {
+        console.log(profile)
           bcrypt.hashPassword(profile.id).then((id) => {
               Model.user.findOrCreate({where : {
                     profileURL : profile._json.link
